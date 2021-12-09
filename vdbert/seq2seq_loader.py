696cb2908d2759a103626d951214dab21fa5d19d
Satwik Kottur <satwik27@gmail.com>
2021-12-09 12:24:35 -0800

    Adds reading images from different source

--- vdbert/seq2seq_loader.py
+++ vdbert/seq2seq_loader.py
@@ -14,8 +14,128 @@ import pickle
 from typing import List
 import copy
 
+# Using different feature source.
+import lmdb
+import base64
+
 
 class ImageFeaturesHdfReader(object):
+    """
+    A reader for HDF files containing pre-extracted image features. A typical
+    HDF file is expected to have a column named "image_id", and another column
+    named "features".
+
+    Example of an HDF file:
+    ```
+    visdial_train_faster_rcnn_bottomup_features.h5
+       |--- "image_id" [shape: (num_images, )]
+       |--- "features" [shape: (num_images, num_proposals, feature_size)]
+       +--- .attrs ("split", "train")
+    ```
+    Refer ``$PROJECT_ROOT/data/extract_bottomup.py`` script for more details
+    about HDF structure.
+
+    Parameters
+    ----------
+    features_hdfpath : str
+        Path to an HDF file containing VisDial v1.0 train, val or test split
+        image features.
+    in_memory : bool
+        Whether to load the whole HDF file in memory. Beware, these files are
+        sometimes tens of GBs in size. Set this to true if you have sufficient
+        RAM - trade-off between speed and memory.
+    """
+
+    def __init__(self, features_hdfpath: str, in_memory: bool = False):
+        self.features_hdfpath = features_hdfpath
+        self._in_memory = in_memory
+
+        self.env = lmdb.open(
+            features_hdfpath,
+            max_readers=1,
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False
+        )
+
+        with self.env.begin(write=False) as file_ptr:
+            self._image_id_list = pickle.loads(file_ptr.get('keys'.encode()))
+            # "features" is List[np.ndarray] if the dataset is loaded in-memory
+            # If not loaded in memory, then list of None.
+            self.features = [None] * len(self._image_id_list)
+            self.boxes = [None] * len(self._image_id_list)
+            self.classes = [None] * len(self._image_id_list)
+            self.scores = [None] * len(self._image_id_list)
+
+    def __len__(self):
+        return len(self._image_id_list)
+
+    def __getitem__(self, image_id: int):
+        image_id = str(image_id).encode()
+        index = self._image_id_list.index(image_id)
+        if self._in_memory:
+            # Load features during first epoch, all not loaded together as it
+            # has a slow start.
+            if self.features[index] is not None:
+                image_id_features = self.features[index]
+                boxes = self.boxes[index]
+                single_class = self.classes[index]
+                single_score = self.scores[index]
+
+            else:
+                with self.env.begin(write=False) as file_ptr:
+                    item = pickle.loads(file_ptr.get(image_id))
+                    num_boxes = int(item["num_boxes"])
+                    image_id_features = np.frombuffer(
+                        base64.b64decode(item["features"]), dtype=np.float32
+                    ).reshape(num_boxes, 2048)
+                    boxes = np.frombuffer(
+                        base64.b64decode(item["boxes"]), dtype=np.float32
+                    ).reshape(num_boxes, 4)
+                    class_probs = np.frombuffer(
+                        base64.b64decode(item["cls_prob"]), dtype=np.float32
+                    ).reshape(num_boxes, 1601)
+
+                    single_class = np.argmax(class_probs, axis=1)
+                    single_score = np.max(class_probs, axis=1)
+                    self.features[index] = image_id_features
+                    self.boxes[index] = boxes
+                    self.classes[index] = single_class
+                    self.scores[index] = single_score
+        else:
+            # Read chunk from file everytime if not loaded in memory.
+            with self.env.begin(write=False) as file_ptr:
+                item = pickle.loads(file_ptr.get(image_id))
+                num_boxes = int(item["num_boxes"])
+                image_id_features = np.frombuffer(
+                    base64.b64decode(item["features"]), dtype=np.float32
+                ).reshape(num_boxes, 2048)
+                boxes = np.frombuffer(
+                    base64.b64decode(item["boxes"]), dtype=np.float32
+                ).reshape(num_boxes, 4)
+                class_probs = np.frombuffer(
+                    base64.b64decode(item["cls_prob"]), dtype=np.float32
+                ).reshape(num_boxes, 1601)
+
+                single_class = np.argmax(class_probs, axis=1)
+                single_score = np.max(class_probs, axis=1)
+                self.features[index] = image_id_features
+                self.boxes[index] = boxes
+                self.classes[index] = single_class
+                self.scores[index] = single_score
+
+        return image_id_features, boxes, single_class, single_score
+
+    def keys(self) -> List[int]:
+        return self._image_id_list
+
+    @property
+    def split(self):
+        return self._split
+
+
+class ImageFeaturesHdfReaderOriginal(object):
     """
     A reader for HDF files containing pre-extracted image features. A typical
     HDF file is expected to have a column named "image_id", and another column
